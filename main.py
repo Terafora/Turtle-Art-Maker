2b6088a4f95811d6359f378a452ff5e86b49fbe3
Charlotte <charlie.stone649@gmail.com>
2023-10-21 05:53:58 +0100

    Set up area for first challenge of drawing a square with the turtle module

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
+brush = Turtle()
+brush.shape("arrow")
+brush.pencolor("#b342f5")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
